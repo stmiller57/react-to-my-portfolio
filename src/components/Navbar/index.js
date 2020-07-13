a3b8240951b04749384304b018d45cdac89cca53
Reformed Scribe <stmiller57@gmail.com>
2020-07-13 07:28:13 -0400

    Created React App, added background color and desired font, created Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+            <a className="navbar-brand" href="#">Stephen Miller</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-item nav-link" href="index.html">About</a>
+                    <a className="nav-item nav-link" href="#coding-portfolio">Coding portfolio</a>
+                    <a className="nav-item nav-link" href="#recent-writing">Selected writing</a>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
