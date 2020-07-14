a89709474bb66369bac6e20a542c07c79ca746c2
Reformed Scribe <stmiller57@gmail.com>
2020-07-14 09:29:52 -0400

    Created Wrapper component for ProjectCards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import BioBox from './components/BioBox';
 import ProjectCard from "./components/ProjectCard";
+import Wrapper from "./components/Wrapper";
 import projects from "./components/projects.json";
 
 function App() {
@@ -10,22 +11,24 @@ function App() {
     <div className="App">
       <Header />
       <BioBox />
-      <ProjectCard
-        image={projects[0].image}
-        title={projects[0].title}
-        body={projects[0].body}
-        technology={projects[0].technology}
-        link={projects[0].link}
-        github={projects[0].github}
-      />
-      <ProjectCard
-        image={projects[1].image}
-        title={projects[1].title}
-        body={projects[1].body}
-        technology={projects[1].technology}
-        link={projects[1].link}
-        github={projects[1].github}
-      />
+      <Wrapper>
+        <ProjectCard
+          image={projects[0].image}
+          title={projects[0].title}
+          body={projects[0].body}
+          technology={projects[0].technology}
+          link={projects[0].link}
+          github={projects[0].github}
+        />
+        <ProjectCard
+          image={projects[1].image}
+          title={projects[1].title}
+          body={projects[1].body}
+          technology={projects[1].technology}
+          link={projects[1].link}
+          github={projects[1].github}
+        />
+      </Wrapper>
       <Footer />
 
     </div>
