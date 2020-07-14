c266809582902b9097ae0cc33f26f42388f627bd
Reformed Scribe <stmiller57@gmail.com>
2020-07-14 15:47:18 -0400

    Added remaining projects to portfolio page

--- src/App.js
+++ src/App.js
@@ -31,6 +31,42 @@ function App() {
           github={projects[1].github}
         />
       </Wrapper>
+      <Wrapper>
+        <ProjectCard
+          image={projects[2].image}
+          title={projects[2].title}
+          body={projects[2].body}
+          technology={projects[2].technology}
+          link={projects[2].link}
+          github={projects[2].github}
+        />
+        <ProjectCard
+          image={projects[3].image}
+          title={projects[3].title}
+          body={projects[3].body}
+          technology={projects[3].technology}
+          link={projects[3].link}
+          github={projects[3].github}
+        />
+      </Wrapper>
+      <Wrapper>
+        <ProjectCard
+          image={projects[4].image}
+          title={projects[4].title}
+          body={projects[4].body}
+          technology={projects[4].technology}
+          link={projects[4].link}
+          github={projects[4].github}
+        />
+        <ProjectCard
+          image={projects[5].image}
+          title={projects[5].title}
+          body={projects[5].body}
+          technology={projects[5].technology}
+          link={projects[5].link}
+          github={projects[5].github}
+        />
+      </Wrapper>
       <Footer />
 
     </div>
