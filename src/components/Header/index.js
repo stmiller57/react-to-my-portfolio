9963d066333195af336257fdf2d02396db268413
Reformed Scribe <stmiller57@gmail.com>
2020-07-15 07:03:42 -0400

    Updated navbar elements and routes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,33 +5,23 @@ import "./style.css";
 function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-            <Link
-                to="/"
-                className={
-                    window.location.pathname === "/" ||
-                        window.location.pathname === "/portfolio"
-                        ? "nav-link active"
-                        : "nav-link"
-                }>
-                <h2>Stephen Miller</h2>
-            </Link>
+            <Link to="/portfolio" className={window.location.pathname === "/"
+                ? "nav-link active" : "nav-link"
+            }><h2>Stephen Miller</h2></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link" href="#coding-portfolio">Coding portfolio</a>
+                <div className="navbar-nav ml-auto">
+                    <Link to="/portfolio" className={window.location.pathname === "/portfolio"
+                        ? "nav-link active" : "nav-link"
+                    }>Portfolio</Link>
                     <Link
                         to="/writing"
-                        className={
-                            window.location.pathname === "/" ||
-                                window.location.pathname === "/writing"
-                                ? "nav-link active"
-                                : "nav-link"
-                        }
-                    >
-                        Selected writing                </Link>
+                        className={window.location.pathname === "/writing"
+                            ? "nav-link active" : "nav-link"
+                        }>Selected writing</Link>
                 </div>
             </div>
         </nav>
