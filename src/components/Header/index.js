a83d0adfdae2f4e8387d0e667136f32dfc012ecd
Reformed Scribe <stmiller57@gmail.com>
2020-07-16 08:59:36 -0400

    Made more adjustments to Navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,9 +6,7 @@ function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-success">
             <Link to="/" className={window.location.pathname === "/"
-                ? "nav-link active" : "nav-link"
-            }><img src="./assets/logo.png" alt="Stephen Miller" className="img-responsive"
-                width="20%" /></Link>
+            }><img src="./assets/logo.png" alt="Stephen Miller" className="img-responsive" width="20%" /></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
