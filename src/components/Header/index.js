0bb683b1e00e7e2c0398794e9fb84ad50c170de4
Reformed Scribe <stmiller57@gmail.com>
2020-07-16 08:06:14 -0400

    Finished styling portfolio, added logo in Navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,13 +5,10 @@ import "./style.css";
 function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-            <svg width="50px" height="50px" viewBox="0 0 16 16" className="bi bi-laptop text-white" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M13.5 3h-11a.5.5 0 0 0-.5.5V11h12V3.5a.5.5 0 0 0-.5-.5zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11z" />
-                <path d="M0 12h16v.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5V12z" />
-            </svg>
-            <Link to="/portfolio" className={window.location.pathname === "/"
+            <Link to="/" className={window.location.pathname === "/"
                 ? "nav-link active" : "nav-link"
-            }><h2>Stephen Miller</h2></Link>
+            }> <a href="mailto:samiller57@gmail.com"><img src="./assets/logo.png" alt="Phone" className="img-responsive"
+                width="20%" /></a></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
