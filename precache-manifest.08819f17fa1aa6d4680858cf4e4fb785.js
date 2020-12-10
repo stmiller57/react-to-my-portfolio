0e1cdd30623f16d5d1c8104cf572b34924c5e8db
Reformed Scribe <stmiller57@gmail.com>
2020-12-10 11:03:52 -0500

    Updates

---
+++ precache-manifest.08819f17fa1aa6d4680858cf4e4fb785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17867e73398082ded168a520dad819e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00eee3e99b0fb3f8d975",
+    "url": "/static/css/main.376b8279.chunk.css"
+  },
+  {
+    "revision": "9d50291951b334afad0b",
+    "url": "/static/js/2.4d35badd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4d35badd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00eee3e99b0fb3f8d975",
+    "url": "/static/js/main.4b081903.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
